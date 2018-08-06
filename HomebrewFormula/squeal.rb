a0054d439a9267afd025791fee34a3c146d52e04
sanjiv sahayam <sanjsmailbox@gmail.com>
2018-08-06 23:11:25 +1000

    release version 0.1.0.8

--- HomebrewFormula/squeal.rb
+++ HomebrewFormula/squeal.rb
@@ -1,9 +1,9 @@
 class Squeal < Formula
   desc "Test for pwned passwords and emails"
   homepage "https://github.com/ssanj/squeal"
-  version "0.1.0.1"
+  version "0.1.0.8"
   url "https://github.com/ssanj/squeal/releases/download/v#{version}/squeal-v#{version}-osx.tar.gz"
-  sha256 "d6f0c4113daec4621e22edb5d41f81815343b7d626df1b306b7bb7e715f74a56"
+  sha256 "3174aec0b0a28e4b8c8ed009a3ef76112be544778dd20338742319b7a0a05d18"
   head "https://github.com/ssanj/squeal.git"
 
   def install
